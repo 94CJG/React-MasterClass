2e09e43a364870cfaa014884fc3cee8194c36972
94CJG <krsoi9412@gmail.com>
2023-07-08 14:15:01 +0900

    styled-components의 오브젝트를 사용하여 css 변경함.
    ㅁas를 사용하여 엘리먼트를 다른 엘리먼트로 교체가 가능하다.
    ㅁstyled-components는 오브젝트를 사용하여 옵션들을 넣을 수 있다.
    동시에 여러개도 가능하다. 왜? 오브젝트로 구분하여 넣어준다.
    ㅁ

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import { styled } from 'styled-components';
 const Father = styled.div`
   display: flex;
 `;
-const Box = styled.div`
-  background-color: ${(props) => (props.bgcolor)};
-  width: 100px;
-  height: 100px;
-`;
 
-const Circle = styled(Box) `
-  border-radius: 50px;
+const Input = styled.input.attrs({ required: true, minlength: 10 })`
+  //오브젝트를 담을 수 있다.
+  //현재 required를 Input에 일괄적으로 넣고 싶을때 사용 할 수있다.
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgcolor="red" />
-      <Circle bgcolor="orange" />
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
