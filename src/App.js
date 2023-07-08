6ab7a63aaa40625995fa36b340e31a1474a05363
94CJG <krsoi9412@gmail.com>
2023-07-08 16:09:59 +0900

    에니메이션 사용방법
    ㅁ 스타일 컴포넌트 에니메이션(keyframes)는 import를 하여 사용한다.
    ㅁ 부모 스타일 컴포넌트 안에 포함 되어 있으면 스타일을 변경시 그 안에 작성이 가능하다.
    ㅁ span:hover = &:hover 같은 의미이다. & = span
    ㅁ style을 꾸며줄 때 모든게 다 component 할 필요가 없다.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
-import { styled } from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+ //에니메이션을 사용하기 위한 import 추가
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({ required: true, minlength: 10 })`
+const rotatinAnimation = keyframes` //노말 CSS코드를 작성
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    transform: rotate(360deg);
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: ${rotatinAnimation} 2s linear infinite;
+  //에니메이션을 사용 하는 방법 JavaScript string을 작성
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    // 부모 컴포넌트 안에 있기 때문에 CSS 변경시 이렇게 작성 가능함.
+    font-size: 36px;
+    &:hover {
+      font-size: 40px;
+      // span:hover와 같은 의미 이다. &=sapn 이라는 의미.
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+  //결국 style를 꾸며줄 때 모든 게 다 component일 필요가 없다.
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>😍</span> {/**스타일 컴포넌트가 아니다. */}
+      </Box>
+    </Wrapper>
   );
-};
-export default App
+}
+export default App;
